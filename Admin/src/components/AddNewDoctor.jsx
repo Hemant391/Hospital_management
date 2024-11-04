1029225f645e8caa31533e7d30f3b7e3918c35f2
Hemant391 <148564538+Hemant391@users.noreply.github.com>
2024-11-04 12:18:09 +0530

    add new doctor

--- Admin/src/components/AddNewDoctor.jsx
+++ Admin/src/components/AddNewDoctor.jsx
@@ -1,4 +1,4 @@
-import  { useContext, useState } from "react";
+import { useContext, useState } from "react";
 import { Navigate, useNavigate } from "react-router-dom";
 import { toast } from "react-toastify";
 import { Context } from "../main";
@@ -58,7 +58,7 @@ const AddNewDoctor = () => {
       formData.append("doctorDepartment", doctorDepartment);
       formData.append("docAvatar", docAvatar);
       await axios
-        .post("https://hospital-management-backend-2w2h.onrender.com/api/v1/user/doctor/addnew", formData, {
+        .post("http://localhost:4000/api/v1/user/doctor/addnew", formData, {
           withCredentials: true,
           headers: { "Content-Type": "multipart/form-data" },
         })
